7f3aa3afb97b98e61e2a905e1cf519f8a5a39cb2
Michael Waters <mwaters@csumb.edu>
2018-09-20 22:28:05 +0000

    Fixing a directory address.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
       <h2>Lab 1:</h2>
       <a href = "Labs/portfolio/index.html">Lab 1: Portfolio</a>
       <h2>Lab 2:</h2>
-      <a href = "Labs/Lab2/index.php">Lab 2: Slot Machine</a>
+      <a href = "Labs/Lab2/777/index.php">Lab 2: Slot Machine</a>
       <h1>Homework</h1>
       
     </body>
