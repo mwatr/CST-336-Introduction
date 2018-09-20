78a1f89c0adf81b9c73e948c826ec63d141706d6
Michael Waters <mwaters@csumb.edu>
2018-09-20 18:39:11 +0000

    Fixing my Heroku Links

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+      <h1>Labs</h1>
+      <h2>Lab 1:</h2>
+      <a href = "Labs/portfolio/index.html">Lab 1: Portfolio</a>
+      <h2>Lab 2:</h2>
+      <a href = "Labs/Lab2/index.php">Lab 2: Slot Machine</a>
+      <h1>Homework</h1>
+      
+    </body>
+</html>
